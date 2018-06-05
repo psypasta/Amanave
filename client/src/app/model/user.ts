c8e86beccbb1a021938c5ff8ea381d3970939ea3
Jonvers <jonathan.werner@hotmail.com>
2018-06-05 20:37:57 +0200

    Revert push fail

---
+++ client/src/app/model/user.ts
@@ -0,0 +1,7 @@
+
+export class User {
+  username: string;
+  name: string;
+  email: string;
+  password: string;
+}
---
+++ client/src/app/service/user.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import {User} from "../model/user";
+
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  userURL = 'http://192.168.0.69:5000/api/auth/';
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  addUser(user: User): Observable<any>{
+   return this.http.post<User>(this.userURL + "signup", user, httpOptions).pipe(
+   )
+  }
+}
