6e02b40c6f23356ce494e8731ca935aa1fd0f437
fulsomenko <max.blomstervall@gmail.com>
2018-06-10 07:17:43 +0200

    removed some testing codes

--- client/src/app/signup/signup.component.ts
+++ client/src/app/signup/signup.component.ts
@@ -17,9 +17,6 @@ export class SignUpComponent {
 
 	onSubmit() { this.submitted = true; }
 
-	// TODO: Remove this when we're done
-	get diagnostic() { return JSON.stringify(this.model); }
-
 	newUser(){
 	    console.log(this.loginCred);
 	    console.log(this.user);
