b859ff635e6d826730f8cdad35efdaf7355362fa
fulsomenko <max.blomstervall@gmail.com>
2018-06-13 11:01:13 +0200

    order model fields added

--- client/src/app/model/order.ts
+++ client/src/app/model/order.ts
@@ -1,2 +1,7 @@
 export class Order {
+
+  job: string;
+  quantity: number[];
+  products: number[];
+
 }
--- inventory/src/main/java/com/example/inventory/controller/OrderResource.java
+++ inventory/src/main/java/com/example/inventory/controller/OrderResource.java
@@ -12,10 +12,7 @@ import com.example.inventory.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
