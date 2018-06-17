2bf460456c58d53e68844eeb977ac7b2522f59cf
fulsomenko <max.blomstervall@gmail.com>
2018-06-17 18:36:45 +0200

    sluta byt lösen och getter och setter för quantity

--- inventory/src/main/java/com/example/inventory/model/OrderDetails.java
+++ inventory/src/main/java/com/example/inventory/model/OrderDetails.java
@@ -67,4 +67,12 @@ public class OrderDetails {
     public void setProduct(Product product) {
         this.product = product;
     }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
